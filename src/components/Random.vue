9649e6aa03b496ff7b30557c66ba8bdb4bcf9c40
OriolB-Uri <oriolbotella@hotmail.com>
2023-11-15 14:06:08 +0100

    iteracion 1 y 2 hechas

---
+++ src/components/Random.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps(['min', 'max'])
+</script>
+
+<template>
+    <div>
+        <!-- Corrregir 13:51. calucular un número aleatorio entre min y max y colocarlo donde la Z -->
+        Random value between {{ min }} and {{ max }} => {{ Math.floor(Math.random() * (max - min + 1)) + min }}
+    </div>
+</template>
+
+<style scoped>
+div {
+    border: 1px solid black;
+    max-width: 400px;
+}
+</style>
