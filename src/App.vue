f7cb4d1fa8169e359e2cf39e39737ee74c1eefab
OriolB-Uri <oriolbotella@hotmail.com>
2023-11-17 10:58:28 +0100

    like button hecho

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <script setup>
-import IdCard from './components/IdCard.vue';
-import Random from './components/Random.vue';
-import BoxColor from './components/BoxColor.vue'
 
+import { ref, computed } from 'vue';
+
+import IdCard from './components/IdCard.vue'
+import Random from './components/Random.vue'
+import BoxColor from './components/BoxColor.vue'
+import LikeButton from './components/LikeButton.vue';
 </script>
 
 <template>
-  <h1>Creación de componentes con Vue</h1><br>
+  <h1>Creación de componentes con Vue</h1>
   <section>
-    <h2>Id Card</h2>
-    <IdCard last-name='Doe' first-name='John' gender='male' :height="178" :birth='new Date("1992-07-14")'
-      picture="https://randomuser.me/api/portraits/men/44.jpg" />
-    <IdCard last-name='Delores ' first-name='Obrien' gender='female' :height="172" :birth='new Date("1988-05-11")'
-      picture="https://randomuser.me/api/portraits/women/44.jpg" />
 
-      <h2>Random</h2>
-    <Random :min="1" :max="6" />
-    <Random :min="1" :max="100" />
-    <Random :min="100" :max="1000" />
-
+    <LikeButton />
+    <LikeButton />
     <h2>BoxColor</h2>
     <BoxColor :r="255" :g="0" :b="0" />
     <BoxColor :r="128" :g="255" :b="0" />
     <BoxColor :r="50" :g="50" :b="50" />
     <BoxColor :r="0" :g="0" :b="0" />
     <BoxColor :r="255" :g="129" :b="129" />
+
+
+    <h2>Random</h2>
+    <Random :min="1" :max="6" />
+    <Random :min="1" :max="100" />
+    <Random :min="100" :max="1000" />
+
+    <h2>Id Card</h2>
+    <IdCard last-name='Doe' first-name='John' gender='male' :height="178" :birth='new Date("1992-07-14")'
+      picture="https://randomuser.me/api/portraits/men/44.jpg" />
+    <IdCard last-name='Delores ' first-name='Obrien' gender='female' :height="172" :birth='new Date("1988-05-11")'
+      picture="https://randomuser.me/api/portraits/women/44.jpg" />
 
   </section>
 </template>
